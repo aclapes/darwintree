fixed numerical problems using Adrien's functions

classification.py
@@ -7,42 +7,83 @@ from os.path import isfile, exists
 from sklearn import svm
 from sklearn.metrics import accuracy_score, average_precision_score
 import sys
+from os import makedirs
+import time
 
 
 INTERNAL_PARAMETERS = dict(
     weights = None
 )
 
-def classify_using_bovwtrees(feats_path, videonames, class_labels, train_test_idx, feat_types):
+def classify_using_bovwtrees(feats_path, videonames, class_labels, train_test_idx, feat_types, w, classification_path):
+    if not exists(classification_path):
+        makedirs(classification_path)
+
     # process videos
     total = len(videonames)
 
-    kernels_tr = []
-    kernels_te = []
+    kernels_train = []
+    kernels_test = []
     for feat_t in feat_types:
-        bowtrees = []
-        for i in xrange(total):
-            input_filepath = join(feats_path, feat_t, videonames[i] + '-bovwtree.pkl')
-            if not isfile(input_filepath):
-                sys.stderr.write('# WARNING: missing training instance'
-                                 ' {}\n'.format(input_filepath))
-                sys.stderr.flush()
-                continue
-
-            with open(input_filepath) as f:
+        train_filepath = join(classification_path, 'bowtrees_ATEP_train-' + feat_t + '.pkl')
+        test_filepath = join(classification_path, 'bowtrees_ATEP_test-' + feat_t + '.pkl')
+        if isfile(train_filepath) and isfile(test_filepath):
+            with open(train_filepath, 'rb') as f:
                 data = cPickle.load(f)
+                Kr_train, Ke_train = data['Kr_train'], data['Ke_train']
+            with open(test_filepath, 'rb') as f:
+                data = cPickle.load(f)
+                Kr_test, Ke_test = data['Kr_test'], data['Ke_test']
+        else:
+            try:
+                with open(classification_path + 'bowtrees-' + feat_t + '.pkl', 'rb') as f:
+                    bowtrees = cPickle.load(f)
+            except IOError:
+                bowtrees = []
+                for i in xrange(total):
+                    input_filepath = join(feats_path, feat_t, videonames[i] + '-bovwtree.pkl')
+                    if not isfile(input_filepath):
+                        sys.stderr.write('# WARNING: missing training instance'
+                                         ' {}\n'.format(input_filepath))
+                        sys.stderr.flush()
+                        continue
+                    with open(input_filepath) as f:
+                        data = cPickle.load(f)
 
-            t = get_bovwtree(data)
-            bowtrees.append(t)
+                    t = get_bovwtree(data)
+                    bowtrees.append(t)
 
-        K_tr = compute_ATEP_kernel(bowtrees_tr=bowtrees[train_test_idx[0]], w=0.5)
-        K_te = compute_ATEP_kernel(bowtrees_tr=bowtrees[train_test_idx[0]], bowtrees_te=bowtrees[train_test_idx[1]], w=0.5)
+                with open(classification_path + 'bowtrees-' + feat_t + '.pkl', 'wb') as f:
+                    cPickle.dump(bowtrees, f)
 
-        feat_norm = 1.0 / np.mean(K_tr)
-        kernels_tr.append(feat_norm * K_tr)
-        kernels_te.append(feat_norm * K_te)
+            bowtrees = np.array(bowtrees)
 
-    results = _train_and_classify(kernels_tr, kernels_te, feat_types, class_labels, train_test_idx)
+            try:
+                with open(train_filepath, 'rb') as f:
+                    data = cPickle.load(f)
+                    Kr_train, Ke_train = data['Kr_train'], data['Ke_train']
+            except IOError:
+                Kr_train, Ke_train = compute_ATEP_kernel(bowtrees_tr=bowtrees[train_test_idx[0]])
+                with open(train_filepath, 'wb') as f:
+                    cPickle.dump(dict(Kr_train=Kr_train, Ke_train=Ke_train), f)
+
+            try:
+                with open(test_filepath, 'rb') as f:
+                    data = cPickle.load(f)
+                    Kr_test, Ke_test = data['Kr_test'], data['Ke_test']
+            except IOError:
+                Kr_test, Ke_test = compute_ATEP_kernel(bowtrees_tr=bowtrees[train_test_idx[0]], bowtrees_te=bowtrees[train_test_idx[1]])
+                with open(test_filepath, 'wb') as f:
+                    cPickle.dump(dict(Kr_test=Kr_test, Ke_test=Ke_test), f)
+
+        K_train = w*Kr_train + (1-w)*Ke_train
+        feat_norm = 1.0 / np.mean(K_train)
+        kernels_train.append(feat_norm * K_train)
+
+        K_test = w*Kr_test + (1-w)*Ke_test
+        kernels_test.append(feat_norm * K_test)
+
+    results = _train_and_classify(kernels_train, kernels_test, feat_types, class_labels, train_test_idx)
     return results
 
 
@@ -67,33 +108,42 @@ def get_bovwtree(data):
 
 
 def compute_ATEP_kernel(bowtrees_tr, bowtrees_te=None, w=0.5):
-    if bowtrees_te is None:
+
+    for i in range(0, len(bowtrees_tr)):
+        bowtrees_tr[i][0] = bowtrees_tr[i][0].astype('float')
+        for edge_i in range(0, len(bowtrees_tr[i][1])):
+            bowtrees_tr[i][1][edge_i] = np.concatenate(bowtrees_tr[i][1][edge_i]).astype('float')
+
+    if bowtrees_te is not None:
+        for i in range(0, len(bowtrees_te)):
+            bowtrees_te[i][0] = bowtrees_te[i][0].astype('float')
+            for edge_i in range(0, len(bowtrees_te[i][1])):
+                bowtrees_te[i][1][edge_i] = np.concatenate(bowtrees_te[i][1][edge_i]).astype('float')
+    else:
         bowtrees_te = bowtrees_tr
 
     # init the kernel
-    K = np.zeros((len(bowtrees_tr),len(bowtrees_te)), dtype=np.float32)
+    Kr = np.zeros((len(bowtrees_tr),len(bowtrees_te)), dtype=np.float32)
+    Ke = np.zeros((len(bowtrees_tr),len(bowtrees_te)), dtype=np.float32)
 
     for i in range(0,len(bowtrees_tr)):
         ptr = i if bowtrees_te is None else 0
         for j in range(ptr,len(bowtrees_te)):
             # intersection between root histograms
-            kr = intersection( bowtrees_tr[i][0], bowtrees_te[j][0] )
+            Kr[i,j] = intersection( bowtrees_tr[i][0], bowtrees_te[j][0] )
 
             # pair-wise intersection of edges' histograms
             sum_edges = 0.0
             for edge_i in range(0,len(bowtrees_tr[i][1])):
                 for edge_j in range(0,len(bowtrees_te[j][1])):
-                    sum_edges += intersection( np.concatenate(bowtrees_tr[i][1][edge_i]), np.concatenate(bowtrees_te[j][1][edge_j]) )
-            ke = (1.0/len(bowtrees_tr[i])*len(bowtrees_te[j])) * sum_edges
-
-            # final score is a combination of root similary and pairwise edges' similarities
-            # w = 1 would be the case of classifiying using global representation and intersection kernel
-            K[i,j] = w*kr + (1-w)*ke
+                    sum_edges += intersection( bowtrees_tr[i][1][edge_i], bowtrees_te[j][1][edge_j] )
+            Ke[i,j] = (1.0/len(bowtrees_tr[i])*len(bowtrees_te[j])) * sum_edges
 
     if bowtrees_te is None:
-        K = K + np.triu(K,1).T
+        Kr = Kr + np.triu(Kr,1).T
+        Ke = Ke + np.triu(Ke,1).T
 
-    return K
+    return Kr, Ke
 
 
 def _train_and_classify(kernels_tr, kernels_te, feat_types, class_labels, train_test_idx):
@@ -128,7 +178,7 @@ def _train_and_classify_binary(K_tr, K_te, train_labels, test_labels):
     # K_te = np.hstack((one_to_n[:,np.newaxis], K_te))
 
     # Train
-    c_param = 100
+    c_param = 1
     clf = svm.SVC(kernel='precomputed', C=c_param, max_iter=-1, tol=1e-3)
     clf.fit(K_tr, train_labels)
 
@@ -136,15 +186,20 @@ def _train_and_classify_binary(K_tr, K_te, train_labels, test_labels):
     test_preds = clf.predict(K_te)
 
     # Compute accuracy and average precision
-    acc = accuracy_score(test_labels, test_preds)
-    ap = average_precision_score(test_labels, test_preds)
+    cmp = test_labels == test_preds
+    neg_acc = float(np.sum(cmp[test_labels <= 0]))/len(test_labels[test_labels <= 0])
+    pos_acc = float(np.sum(cmp[test_labels > 0]))/len(test_labels[test_labels > 0])
+    acc = (pos_acc + neg_acc) / 2.0
+
+    # TODO: compute ap per class
+    ap = average_precision_score(test_labels, test_preds, average='weighted')
 
     return acc, ap
 
 
 def l1normalize(x):
     x_norm = np.sum(np.abs(x))
-    return x.astype('float') / x_norm
+    return x / x_norm
 
 
 def intersection(x1, x2):

main.py
@@ -54,8 +54,9 @@ def set_global_config():
     intermediates_path = parent_path + 'intermediates/'
     feats_path = parent_path + 'feats/'
     darwins_path = parent_path + 'darwins/'
+    classification_path = parent_path + 'classification/'
 
-    return tracklets_path, clusters_path, intermediates_path, feats_path, darwins_path
+    return tracklets_path, clusters_path, intermediates_path, feats_path, darwins_path, classification_path
 
 
 def set_dataset_config(dataset_name):
@@ -282,7 +283,7 @@ def train_and_classify(K_tr, K_te, train_labels, test_labels):
 
 
 if __name__ == "__main__":
-    tracklets_path, clusters_path, intermediates_path, feats_path, darwins_path = set_global_config()
+    tracklets_path, clusters_path, intermediates_path, feats_path, darwins_path, classification_path = set_global_config()
     # load dataset configuration (check README.md, DATASETS section)
     fullvideonames, videonames, class_labels, action_names, train_test_indx = set_dataset_config(INTERNAL_PARAMETERS['dataset_name'])
 
@@ -303,35 +304,35 @@ if __name__ == "__main__":
     print('INSTANCE_ST: %d, INSTANCE_TOTAL: %d' % (INSTANCE_ST, INSTANCE_TOTAL))
 
     tracklet_extraction.extract(fullvideonames, videonames, INSTANCE_ST, INSTANCE_TOTAL, INTERNAL_PARAMETERS['feature_types'], tracklets_path)
-    tracklet_clustering.cluster(tracklets_path, videonames, INSTANCE_ST, INSTANCE_TOTAL, clusters_path)
-
-    quit()
+    tracklet_clustering.cluster(tracklets_path, videonames, INSTANCE_ST, INSTANCE_TOTAL, clusters_path, visualize=False)
 
     train_indx, test_indx = train_test_indx
 
-    # tracklet_representation.train_bovw_codebooks(tracklets_path, videonames, train_indx, INTERNAL_PARAMETERS['feature_types'], intermediates_path)
+    tracklet_representation.train_bovw_codebooks(tracklets_path, videonames, train_indx, INTERNAL_PARAMETERS['feature_types'], intermediates_path)
     # tracklet_representation.train_fv_gmms(tracklets_path, videonames, train_indx, INTERNAL_PARAMETERS['feature_types'], intermediates_path)
+
 
     # tracklet_representation.compute_bovw_representations(tracklets_path, clusters_path, intermediates_path, videonames, \
     #                                                      INSTANCE_ST, INSTANCE_TOTAL, INTERNAL_PARAMETERS['feature_types'], feats_path)
     # tracklet_representation.compute_fv_representations(tracklets_path, clusters_path, intermediates_path, videonames, \
     #                                                    INSTANCE_ST, INSTANCE_TOTAL, INTERNAL_PARAMETERS['feature_types'], feats_path)
 
-    # tracklet_representation.compute_bovwtree_representations(tracklets_path, clusters_path, intermediates_path, videonames, \
-    #                                                          INSTANCE_ST, INSTANCE_TOTAL, INTERNAL_PARAMETERS['feature_types'], feats_path)
+    tracklet_representation.compute_bovw_descriptors(tracklets_path, intermediates_path, videonames, INSTANCE_ST, INSTANCE_TOTAL, \
+                                                     INTERNAL_PARAMETERS['feature_types'], feats_path, \
+                                                     treelike=True, clusters_path=clusters_path)
     # tracklet_representation.compute_fv_representations(tracklets_path, clusters_path, intermediates_path, videonames, \
     #                                                    INSTANCE_ST, INSTANCE_TOTAL, INTERNAL_PARAMETERS['feature_types'], feats_path)
 
-    results = classification.classify_using_bovwtrees(feats_path, videonames, class_labels, train_test_indx, INTERNAL_PARAMETERS['feature_types'])
+    results = classification.classify_using_bovwtrees(feats_path, videonames, class_labels, train_test_indx, INTERNAL_PARAMETERS['feature_types'], 1, classification_path)
 
     print("ACTION_NAME AC mAP")
     for i in xrange(class_labels.shape[1]):
-        print("%s %.2f %.2f" % (INTERNAL_PARAMETERS['action_names'][i], results['acc_classes'][i], results['ap_classes'][i]))
+        print("%s %.2f %.2f" % (action_names[i], results['acc_classes'][i], results['ap_classes'][i]))
     print("%.2f %.2f" % (np.mean(results['acc_classes']), np.mean(results['ap_classes'])))
 
-    # darwintree.darwin(fullfeatnames, INSTANCE_ST, INSTANCE_TOTAL, darwins_path)
-
     quit()  # TODO: remove this for further processing
+
+    # darwintree.darwin(fullfeatnames, INSTANCE_ST, INSTANCE_TOTAL, darwins_path)
 
     # # Get the videodarwin representation
     # channels = ['trj']  # channels = ['trj', 'hog', 'hof', 'mbh']

spectral_division.bak.py
@@ -20,13 +20,12 @@ import sys
 import heapq
 
 import numpy as np
-from scipy import sparse, linalg
+from scipy import sparse
 from scipy.sparse.sparsetools import cs_graph_components
 
 import pyflann
 from sklearn.cluster import MiniBatchKMeans, KMeans
 
-import Queue
 
 # The fixed internal paramaters for clustering
 INTERNAL_PARAMETERS = dict(
@@ -97,7 +96,7 @@ def spectral_embedding_nystrom(AB, ridge=1e-10, nvec=2, copy=True):
     A[np.diag_indices_from(A)] += ridge
     # normalize the components of AB
     b_r = B.sum(axis=1)
-    pinvA = spd_pinv(A)
+    pinvA = spd_pinv(A, check_stability=False)
     d1 = A.sum(axis=1) + b_r
     d2 = np.abs(B.sum(axis=0) + np.dot(np.dot(b_r, pinvA), B))
     # Note: abs not required except when numerical problems
@@ -109,7 +108,7 @@ def spectral_embedding_nystrom(AB, ridge=1e-10, nvec=2, copy=True):
     A *= np.dot(dhat[:n], dhat[:n].T)
     B *= np.dot(dhat[:n], dhat[n:].T)
     # square root of the pseudo-inverse
-    Asi = spd_pinv(A, square_root=True)
+    Asi = spd_pinv(A, square_root=True, check_stability=False)
     # compute the embedding vectors
     AsiB = np.dot(Asi, B)  # XXX time & memory bottleneck (20% of total)
     S = A + np.dot(AsiB, AsiB.T)  # XXX bottleneck (20% of total)
@@ -145,7 +144,7 @@ def spectral_embedding_nystrom(AB, ridge=1e-10, nvec=2, copy=True):
     return E
 
 
-def spectral_clustering_division(E, geoms, split_type="threshold"):
+def spectral_clustering_division(E, geoms, split_type="threshold", normalize_geoms=True):
     """Divisive hierarchical clustering + model selection
 
     Recursively split in two by thresholding the eigenvectors in increasing
@@ -198,9 +197,12 @@ def spectral_clustering_division(E, geoms, split_type="threshold"):
         mts = n_mts
 
     # get the normalized spatio-temporal positions
-    # nrlz = np.array([640., 480., 1e2])
-    ngeoms = geoms.astype(np.float) #/ nrlz[np.newaxis, :]
-    ngeoms -= ngeoms.mean(axis=0)[np.newaxis, :]
+    if normalize_geoms:
+        nrlz = np.array([640., 480., 1e2])
+        ngeoms = geoms.astype(np.float) / nrlz[np.newaxis, :]
+        ngeoms -= ngeoms.mean(axis=0)[np.newaxis, :]
+    else:
+        ngeoms = geoms - geoms.mean(axis=0)[np.newaxis, :]
 
     # initialize the tree structure
     stree = SpectralTree(
@@ -210,52 +212,6 @@ def spectral_clustering_division(E, geoms, split_type="threshold"):
     stree.build()
 
     return stree.labels, stree.int_paths
-
-
-def reconstruct_tree_from_leafs(leafs):
-    """
-    Given a list of leaf, recover all the nodes.
-
-    Parameters
-    ----------
-    leafs:  Leafs are integers, each representing a path in the binary tree.
-            For instance, a leaf value of 5 indicates the leaf is the one
-            reached going throught the folliwing path: root-left-right.
-
-    Returns
-    -------
-    A dictionary indicating for each node a list of all its descendents.
-    Exemple:
-        { 1 : [2,3,4,5,6,7,12,13,26,27],
-          2 : [4,5],
-          3 : [6,7,12,13,26,27],
-          ...
-        }
-    """
-    h = dict()
-    q = Queue.PriorityQueue()
-
-    # recover first intermediate nodes (direct parents from leafs)
-    for path in leafs:
-        parent_path = int(path/2)
-        if not parent_path in h and parent_path > 1:
-            q.put(-parent_path)  # deeper nodes go first (queue reversed by "-")
-        h.setdefault(parent_path, []).append(path)
-
-    # recover other intermediates notes recursevily
-    while not q.empty():
-        path = -q.get()
-        parent_path = int(path/2)
-        if not parent_path in h and parent_path > 1:  # list parent also for further processing
-            q.put(-parent_path)
-
-        h.setdefault(parent_path, [])
-        h[parent_path] += ([path] + h[path])  # append children from current node to their parent
-
-    # update with leafs
-    h.update(dict((i,[i]) for i in leafs))
-
-    return h
 
 
 # ==============================================================================
@@ -302,8 +258,11 @@ def spd_pinv(a, rcond=1e-10, square_root=False, check_stability=True):
     N, _N = a.shape
     assert N == _N, "Matrix is not square!"
     # get the eigen-decomposition
-    w, v = np.linalg.eigh(a)
-
+    # w, v = np.linalg.eigh(a)
+    v, w, u = np.linalg.svd(a)
+    sort_index = np.argsort(w)
+    w = w[sort_index]
+    v = v[:,sort_index]
     # check positive-definiteness
     ev_min = w.min()
     if ev_min <= 0:

tracklet_clustering.py
@@ -6,13 +6,20 @@ import cPickle
 import random
 import time
 from math import isnan
+import sys
 
 import numpy as np
 from sklearn.metrics import pairwise
+from sklearn.decomposition import PCA
 
-from spectral_division import spectral_embedding_nystrom, spectral_clustering_division
+from spectral_division import spectral_embedding_nystrom, spectral_clustering_division, reconstruct_tree_from_leafs, IndefiniteError, NumericalError
 
 import cv2
+
+INTERNAL_PARAMETERS = dict(
+    initial_ridge_value = 1e-10,
+    tries_per_ridge_value = 3
+)
 
 def cluster(tracklets_path, videonames, st, num_videos, clusters_path, visualize=False):
     """
@@ -47,42 +54,69 @@ def cluster(tracklets_path, videonames, st, num_videos, clusters_path, visualize
         for k in xrange(data_obj.shape[0]): # range(0,100):  #
             T = np.reshape(data_trj[k], (data_trj.shape[1]/2,2))  # trajectory features into matrix (time length x 2)
             D.setdefault('x',[]).append( T[1:,0] )  # x's offset + x's relative displacement
-            D.setdefault('y',[]).append( T[1:,1] ) #  y's offset + y's relative displacement
+            D.setdefault('y',[]).append( T[1:,1] )  #  y's offset + y's relative displacement
             D.setdefault('t',[]).append( data_obj[k,0] - np.linspace(T.shape[0]-1, 0, T.shape[0]) )
             D.setdefault('v_x',[]).append( T[1:,0] - T[:-1,0] )
             D.setdefault('v_y',[]).append( T[1:,1] - T[:-1,1] )
 
-        # (Sec. 2.3.1)
-        # A, B = get_tracklet_similarities(D, data_obj[:,7:9])
-        # create a subsample (n << N) stratified by a grid
-        insample, outsample = stratified_subsample_of_tracklets_in_grid(data_obj[:,7:9], p=0.01)  # given tracklets (ending) positions
-        # get the similarities of
-        A, medians = multimodal_product_kernel(D, insample, insample)  # (n), n << N tracklets
-        B, _ = multimodal_product_kernel(D, insample, outsample, medians=medians)  # (N - n) tracklets
-        # (Sec. 2.3.2 and 2.3.3)
-        E_ = spectral_embedding_nystrom(np.hstack((A,B)))
+        ret = False
+        ridge = INTERNAL_PARAMETERS['initial_ridge_value']
+        while not ret:
+            for i in xrange(INTERNAL_PARAMETERS['tries_per_ridge_value']):
+                try:
+                    # (Sec. 2.3.1)
+                    # A, B = get_tracklet_similarities(D, data_obj[:,7:9])
+                    # create a subsample (n << N) stratified by a grid
+                    insample, outsample = stratified_subsample_of_tracklets_in_grid(data_obj[:,7:9], p=0.01)  # given tracklets (ending) positions
+                    # get the similarities of
+                    A, medians = multimodal_product_kernel(D, insample, insample)  # (n), n << N tracklets
+                    B, _ = multimodal_product_kernel(D, insample, outsample, medians=medians)  # (N - n) tracklets
+                    # (Sec. 2.3.2 and 2.3.3)
+                    AB = np.hstack((A,B)).astype('float64')
+                    E_ = spectral_embedding_nystrom(AB, ridge=ridge)
+                    ret = True
+                    break
+                except IndefiniteError:
+                    pass
+                except NumericalError:
+                    pass
+
+            if not ret:
+                # warn the user
+                msg = "WARNING: increasing ridge, {0:.0e} -> {1:.0e}.\n"
+                sys.stderr.write(msg.format(ridge, ridge * 10))
+                sys.stderr.flush()
+                # increase the ridge value
+                ridge *= 10
+
+        # re-organize E rows according to in- and out-sample indices
         E = np.zeros(E_.shape, dtype=E_.dtype)
         E[insample,:] = E_[:len(insample),:]
         E[outsample,:] = E_[len(insample):,:]
-        # (Sec. 2.4)
-        best_labels, int_paths = spectral_clustering_division(E, data_obj[:,7:10], normalize_geoms=False)
 
-        with open(clusters_path + videonames[i] + '.pkl', 'wb') as f:
-            cPickle.dump({'best_labels' : best_labels, 'int_paths' : int_paths}, f)
+        # (Sec. 2.4)
+        best_labels, int_paths = spectral_clustering_division(E, data_obj[:,7:10])
+        tree = reconstruct_tree_from_leafs(np.unique(int_paths))
 
         elapsed_time = time.time() - start_time
         print('%s -> DONE (in %.2f secs)' % (videonames[i], elapsed_time))
 
-        if visualize:
-            n_paths = len(np.unique(int_paths))
-            print '#DEBUG n_labels:', n_paths
+        with open(clusters_path + videonames[i] + '.pkl', 'wb') as f:
+            cPickle.dump({'best_labels' : best_labels, 'int_paths' : int_paths, 'tree' : tree}, f)
 
-            xres = 528
-            yres = 224
+        # DEBUG
+        # -----
+        if visualize:
+            xres, yres = 528, 224
             A = np.zeros((yres,1280,3), dtype=np.uint8)
-            for l,p in enumerate(np.unique(int_paths)):
-                cluster_inds = np.where(int_paths == p)[0]
-                hue = (float(l)/n_paths + random.random()) % 1
+            n_unique_paths = len(np.unique(tree.keys()))
+
+            sorted_keys = sorted(tree.keys(), key=lambda x : x)
+            for i, key in enumerate(sorted_keys):
+                node = tree[key]
+                cluster_inds = np.concatenate([np.where(int_paths == i)[0] for i in node])
+
+                hue = ((float(i)/n_unique_paths) + random.random()) % 1
                 for k in xrange(0, len(cluster_inds)):
                     idx = cluster_inds[k]
                     T = np.reshape(data_trj[idx,:], (data_trj.shape[1]/2,2))
@@ -92,14 +126,16 @@ def cluster(tracklets_path, videonames, st, num_videos, clusters_path, visualize
                         pt2 = (int(T[j,0]*xres-xres/2+t*1280), int(T[j,1]*yres))
                         cv2.line(A, pt1, pt2, hsv_to_rgb((hue,0.5,0.8)), 1)
                     cv2.circle(A, pt2, 1, hsv_to_rgb((hue,0.5,1.0)), -1)
+
                 cv2.imshow("#DEBUG Clustering visualization", A)
+                print key, node
                 cv2.waitKey(0)
+        # -----
 
 
 # ==============================================================================
 # Helper functions
 # ==============================================================================
-
 
 def stratified_subsample_of_tracklets_in_grid(P, nx=3, ny=3, p=0.01):
     """
@@ -115,12 +151,11 @@ def stratified_subsample_of_tracklets_in_grid(P, nx=3, ny=3, p=0.01):
     insample = []
     outsample = []
     for i in range(0,ny):
+        y_ran = (i*(1.0/ny), (i+1)*(1.0/ny))
         for j in range(0,nx):
-            y_ran = (i*(1.0/ny), (i+1)*(1.0/ny))
             x_ran = (j*(1.0/nx), (j+1)*(1.0/nx))
             cell_inds = np.where((P[:,0] >= x_ran[0]) & (P[:,0] < x_ran[1]) & (P[:,1] >= y_ran[0]) & (P[:,1] < y_ran[1]))[0]
             m = len(cell_inds)
-            random.seed(74)
             sorted_inds = sorted(np.arange(m, dtype=np.int32), key=lambda k: np.random.random())
             insample.append(np.array(sorted_inds[:int(np.ceil(m*p_cell))], dtype=np.int32))
             outsample.append(np.array(sorted_inds[int(np.ceil(m*p_cell)):], dtype=np.int32))
@@ -151,32 +186,15 @@ def multimodal_product_kernel(D, primary_inds=None, secondary_inds=None, medians
         D[channel_t] = np.array(D[channel_t], dtype=np.float32)
         X_primary = D[channel_t][primary_inds] if primary_inds is not None else D[channel_t]
         X_secondary = D[channel_t][secondary_inds] if secondary_inds is not None else D[channel_t]
-        S = pairwise.euclidean_distances(X_primary, X_secondary)
+        S = pairwise.pairwise_distances(X=X_primary, Y=X_secondary, metric='euclidean')
         median = np.nanmedian(S[S!=0])
         if len(medians) < len(channels):
             medians.append(median)
-        gamma = 1.0 / (2 * median) if not isnan(median) and median != 0.0 else 0.0
-        K = np.multiply(K, np.exp(-gamma * np.power(S,2))) # rbf kernel and element-wise multiplication
+        gamma = 1.0/(2*median) if not isnan(median) and median != 0.0 else 0.0
+        K_tmp = np.exp(-gamma * np.power(S,2)) # rbf kernel and element-wise multiplication
+        K = np.multiply(K, K_tmp)
+
     return K, medians
-
-
-def get_tracklet_similarities(D, P):
-    """
-    Creates A and B.
-    :param D: dictionary of modalities, each modality is N-by-{#features}
-    :param P: matrix 2-by-N
-    :return A, B:
-    """
-    # create a subsample (n << N) stratified by a grid
-    insample, outsample = stratified_subsample_of_tracklets_in_grid(P, p=0.01)  # given tracklets (ending) positions
-    # get the similarities of
-    A, medians = multimodal_product_kernel(D, insample, insample)  # (n), n << N tracklets
-    B, _ = multimodal_product_kernel(D, insample, outsample, medians=medians)  # (N - n) tracklets
-
-    return A, B
-
-
-
 
 
 def hsv_to_rgb(hsv):