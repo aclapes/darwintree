bugfix on videodarwin computation / changes in directory structures for kernel computation

classification.py
@@ -57,7 +57,7 @@ def classify(kernels, class_labels, traintest_parts, a, feat_types, c=[1]):
 #     print(bar_expr.format(bar_fill, value)),
 
 
-def train_and_classify(kernels_tr, kernels_te, a, feat_types, class_labels, train_test_idx, c=[1], nl=2):
+def train_and_classify(kernels_tr, kernels_te, a, feat_types, class_labels, train_test_idx, c=[1], nl=1):
     '''
 
     :param kernels_tr:
@@ -113,7 +113,7 @@ def train_and_classify(kernels_tr, kernels_te, a, feat_types, class_labels, trai
                             c_j)
                         # TODO: decide what it is
                         Rval_ap[k,i,j] += acc_tmp/skf.n_folds
-                        Rval_ap[k,i,j] += (ap_tmp/skf.n_folds if acc_tmp > 0.5 else 0)
+                        # Rval_ap[k,i,j] += (ap_tmp/skf.n_folds if acc_tmp > 0.5 else 0)
 
             a_bidx, c_bidx = np.unravel_index(Rval_ap[k].argmax(), Rval_ap[k].shape)  # a and c bests' indices
             S[k] = (C[k][0][a_bidx], C[k][1][c_bidx])

main_multithread.py
@@ -74,9 +74,9 @@ def get_global_config(xml_config):
     clusters_path = parent_path + 'clusters/'
     intermediates_path = parent_path + 'intermediates/'
     feats_path = parent_path + 'feats/'
-    darwins_path = parent_path + 'darwins/'
+    kernels_path = parent_path + 'kernels/'
 
-    return tracklets_path, clusters_path, intermediates_path, feats_path, darwins_path
+    return tracklets_path, clusters_path, intermediates_path, feats_path, kernels_path
 
 
 def get_dataset_info(xml_config):
@@ -279,7 +279,7 @@ def print_results(results):
 if __name__ == "__main__":
     # prepare configuration-related variables
     xml_config = load_XML_config('config.xml')  # get it from XML file
-    tracklets_path, clusters_path, intermediates_path, feats_path, darwins_path = get_global_config(xml_config)  # build output dirs
+    tracklets_path, clusters_path, intermediates_path, feats_path, kernels_path = get_global_config(xml_config)  # build output dirs
     fullvideonames, videonames, class_labels, action_names, traintest_parts = get_dataset_info(xml_config)  # build dataset-related variable
 
     # Extract improved dense trajectories
@@ -320,6 +320,18 @@ if __name__ == "__main__":
         print('ATBEP classification (fvtree) took %.2f secs.' % (time.time() - st_time))
         print_results(results)
 
+    if 'atnbep_vd-fv' in xml_config['methods_list']:
+        # tracklet_representation.train_bovw_codebooks(tracklets_path, videonames, traintest_parts, xml_config['features_list'], intermediates_path, pca_reduction=False)
+        # tracklet_representation.compute_bovw_descriptors_multithread(tracklets_path, intermediates_path, videonames, traintest_parts, xml_config['features_list'], \
+        #                                                              feats_path + 'bovwtree/', \
+        #                                                              pca_reduction=False, treelike=True, clusters_path=clusters_path)
+        st_time = time.time()
+        atnbep = kernels.compute_ATNBEP_kernels(feats_path + 'vdtree/', feats_path + 'fvtree/', videonames, traintest_parts, xml_config['features_list'], \
+                                                kernels_path + 'atnbep/', use_disk=False)
+        results = classification.classify(atnbep, class_labels, traintest_parts, np.linspace(0, 1, 11), xml_config['features_list'], c=[0.0001, 0.001, 0.01, 0.1, 1, 10, 100, 1000, 1e4, 1e5, 1e6])
+        print('ATBEP classification (fvtree) took %.2f secs.' % (time.time() - st_time))
+        print_results(results)
+
     # # FV-tree descriptor computation and classification
     # if 'atep_fvtree' in xml_config['methods_list']:
     #     # tracklet_representation.train_fv_gmms(tracklets_path, videonames, traintest_parts, xml_config['features_list'], intermediates_path)
@@ -335,29 +347,29 @@ if __name__ == "__main__":
     #     print_results(results)
     #
     # # Darwin-tree descriptor computation and classification
-    if 'atep_darwintree' in xml_config['methods_list']:
+    if 'atep_vdtree' in xml_config['methods_list']:
         tracklet_representation.train_fv_gmms(tracklets_path, videonames, traintest_parts, xml_config['features_list'], intermediates_path)
         tracklet_representation.compute_vd_descriptors_multithread(tracklets_path, intermediates_path, videonames, traintest_parts, xml_config['features_list'], \
-                                                                   feats_path + 'darwintree/', \
+                                                                   feats_path + 'vdtree/', \
                                                                    treelike=True, clusters_path=clusters_path)
-        quit()
-        atep = kernels.compute_ATEP_kernels(feats_path + 'darwintree/', videonames, traintest_parts, xml_config['features_list'], from_disk=False)
-        results = classification.classify(atep, class_labels, traintest_parts, np.linspace(0, 1, 11), xml_config['features_list'], c=[0.0001, 0.001, 0.01, 0.1, 1, 10, 100, 1000, 1e4, 1e5, 1e6])
-        print('ATEP classification (darwintree) took %.2f secs.' % (time.time() - st_time))
+        st_time = time.time()
+        atep = kernels.compute_ATEP_kernels(feats_path + 'vdtree/', videonames, traintest_parts, xml_config['features_list'], from_disk=False)
+        results = classification.classify(atep, class_labels, traintest_parts, np.linspace(0, 1, 11), xml_config['features_list'], c=[1e-4, 1e-3, 1e-2, 1e-1, 1, 10, 100, 1000, 1e4, 1e5])
+        print('ATEP classification (vdtree) took %.2f secs.' % (time.time() - st_time))
         print_results(results)
 
-    # if 'atep_fvtree+darwintree' in xml_config['methods_list']:
+    # if 'atep_fvtree+vdtree' in xml_config['methods_list']:
     #     # tracklet_representation.train_fv_gmms(tracklets_path, videonames, traintest_parts, xml_config['features_list'], intermediates_path)
     #     # tracklet_representation.compute_vd_descriptors_multithread(tracklets_path, intermediates_path, videonames, traintest_parts, xml_config['features_list'], \
-    #     #                                                            feats_path + 'darwintree/', \
+    #     #                                                            feats_path + 'vdtree/', \
     #     #                                                            treelike=True, clusters_path=clusters_path)
     #     # tracklet_representation.compute_vd_descriptors_multithread(tracklets_path, intermediates_path, videonames, traintest_parts, xml_config['features_list'], \
-    #     #                                                            feats_path + 'darwintree/', \
+    #     #                                                            feats_path + 'vdtree/', \
     #     #                                                            treelike=True, clusters_path=clusters_path)
     #     st_time = time.time()
-    #     results = atep_classification.classify([feats_path + 'fvtree', feats_path + 'darwintree/'], videonames, class_labels, traintest_parts, \
+    #     results = atep_classification.classify([feats_path + 'fvtree', feats_path + 'vdtree/'], videonames, class_labels, traintest_parts, \
     #                                            np.linspace(0, 1, 11), xml_config['features_list'], c=[0.01, 0.1, 1, 10, 100, 1000, 1e4, 1e5, 1e6])
-    #     print('ATEP classification (darwintree) took %.2f secs.' % (time.time() - st_time))
+    #     print('ATEP classification (vdtree) took %.2f secs.' % (time.time() - st_time))
     #     print_results(results)
     #
     # if 'atep_fvdpathtree' in xml_config['methods_list']:

videodarwin.py
@@ -43,9 +43,9 @@ def darwin(X, c_svm_param=1):
     one_to_T = one_to_T[:,np.newaxis]
 
     V = np.cumsum(X,axis=0) / one_to_T
-    u_fow = linearSVR(rootSIFT(V), c_svm_param, 2) # videodarwin
+    u_fow = rootSIFT(linearSVR(V, c_svm_param, 2)) # videodarwin
 
-    V = np.cumsum(np.flipud(X),axis=0) / one_to_T # reverse videodarwin
-    u_rev = linearSVR(rootSIFT(V), c_svm_param, 2)
+    V = rootSIFT(np.cumsum(np.flipud(X),axis=0) / one_to_T) # reverse videodarwin
+    u_rev = linearSVR(V, c_svm_param, 2)
 
     return np.concatenate([u_fow, u_rev])