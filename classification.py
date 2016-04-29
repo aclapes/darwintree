bugfix on weighting a 0 kernel when using multiple mid-levels

classification.py
@@ -30,7 +30,7 @@ rbf_parameters = {'n_estimators': [10,20,30,40],
 #         kernels_train[i]['train']
 
 
-def classify(input_kernels, class_labels, traintest_parts, a, feat_types, strategy='kernel_fusion', C=[1]):
+def classify(input_kernels, class_labels, traintest_parts, a, feat_types, strategy='kernel_fusion', C=[1], opt_criterion='acc'):
     '''
     TODO Fill this.
     :param feats_path:
@@ -49,11 +49,14 @@ def classify(input_kernels, class_labels, traintest_parts, a, feat_types, strate
         kernels_train = input_kernels[k]['train']
         kernels_test  = input_kernels[k]['test']
         if strategy == 'kernel_fusion':
-            results[k] = kernel_fusion_classification(kernels_train, kernels_test, a, feat_types, class_labels, (train_inds, test_inds), C=C)
+            results[k] = kernel_fusion_classification(kernels_train, kernels_test, a, feat_types, class_labels, (train_inds, test_inds), \
+                                                      C=C, opt_criterion=opt_criterion)
         elif strategy == 'simple_voting':
-            results[k] = simple_voting_classification(kernels_train, kernels_test, a, feat_types, class_labels, (train_inds, test_inds), C=C)
+            results[k] = simple_voting_classification(kernels_train, kernels_test, a, feat_types, class_labels, (train_inds, test_inds), \
+                                                      C=C, opt_criterion=opt_criterion)
         elif strategy == 'learning_based_fusion':
-            results[k] = learning_based_fusion_classification(kernels_train, kernels_test, a, feat_types, class_labels, (train_inds, test_inds), C=C)
+            results[k] = learning_based_fusion_classification(kernels_train, kernels_test, a, feat_types, class_labels, (train_inds, test_inds), \
+                                                              C=C, opt_criterion=opt_criterion)
 
     return results
 
@@ -213,7 +216,8 @@ def classify(input_kernels, class_labels, traintest_parts, a, feat_types, strate
 #     return dict(acc_classes=acc_classes, ap_classes=ap_classes)
 
 
-def kernel_fusion_classification(input_kernels_tr, input_kernels_te, a, feat_types, class_labels, train_test_idx, C=[1]):
+def kernel_fusion_classification(input_kernels_tr, input_kernels_te, a, feat_types, class_labels, train_test_idx, \
+                                 C=[1], square_kernels=False, opt_criterion='acc'):
     '''
 
     :param kernels_tr:
@@ -235,7 +239,7 @@ def kernel_fusion_classification(input_kernels_tr, input_kernels_te, a, feat_typ
     # lb = LabelBinarizer(neg_label=-1, pos_label=1)
 
     class_ints = np.dot(class_labels, np.logspace(0, class_labels.shape[1]-1, class_labels.shape[1]))
-    skf = cross_validation.StratifiedKFold(class_ints[tr_inds], n_folds=2, shuffle=False, random_state=74)
+    skf = cross_validation.StratifiedKFold(class_ints[tr_inds], n_folds=4, shuffle=False, random_state=74)
 
     Rval = np.zeros((class_labels.shape[1], len(a), len(C)), dtype=np.float32)
     for k in xrange(class_labels.shape[1]):
@@ -253,11 +257,19 @@ def kernel_fusion_classification(input_kernels_tr, input_kernels_te, a, feat_typ
                     kernels_tr[feat_t] = a_i[2]*np.array(kernels_tr[feat_t]['root']) + (1-a_i[2])*np.array(kernels_tr[feat_t]['nodes'])
 
                 else:
-                    kernels_tr[feat_t]['root']  = [utils.normalize(x[0])
-                                                   for x in kernels_tr[feat_t]['root']]
+                    kernels_tr[feat_t]['root']  = [utils.normalize(x[0] if np.sum(x[0])>0 else kernels_tr[feat_t]['nodes'][j][0])
+                                                   for j,x in enumerate(kernels_tr[feat_t]['root'])]
                     kernels_tr[feat_t]['nodes'] = [utils.normalize(a_i[1]*x[0]+(1-a_i[1])*x[1] if len(x)==2 else x[0])
                                                    for x in kernels_tr[feat_t]['nodes']]
 
+                    # for j,Kr in enumerate(kernels_tr[feat_t]['root']):
+                    #     K = None
+                    #     if np.sum(Kr) > 0:
+                    #         K = a_i[2]*Kr + (1-a_i[2])*kernels_tr[feat_t]['nodes'][j]
+                    #     else:
+                    #         K = kernels_tr[feat_t]['nodes'][j]
+                    #     kernels_tmp.append(K)
+                    # kernels_tr[feat_t] = kernels_tmp
                     kernels_tr[feat_t] = list(a_i[2]*np.array(kernels_tr[feat_t]['root']) + (1-a_i[2])*np.array(kernels_tr[feat_t]['nodes']))
 
             K_tr = None
@@ -267,7 +279,8 @@ def kernel_fusion_classification(input_kernels_tr, input_kernels_te, a, feat_typ
                     K_tr = np.zeros(kernels_tr[feat_t].shape if isinstance(kernels_tr[feat_t],np.ndarray) else kernels_tr[feat_t][0].shape, dtype=np.float32)
                 K_tr += a_i[3][j] * utils.sum_of_arrays(kernels_tr[feat_t], a_i[0])
 
-            # K_tr = np.sqrt(K_tr)
+            if square_kernels:
+                K_tr = np.sqrt(K_tr)
 
             for j, c_j in enumerate(C):
                 # print l, str(i+1) + '/' + str(len(C[k][0])), str(j+1) + '/' + str(len(C[k][1]))
@@ -283,9 +296,11 @@ def kernel_fusion_classification(input_kernels_tr, input_kernels_te, a, feat_typ
                         K_tr[val_tr_inds,:][:,val_tr_inds], K_tr[val_te_msk,:][:,val_tr_inds], \
                         class_labels[tr_inds,k][val_tr_inds], class_labels[tr_inds,k][val_te_msk], \
                         c_j)
-                    # TODO: decide what it is
-                    Rval[k,i,j] += acc_tmp/skf.n_folds
-                    # Rval[k,i,j] += (ap_tmp/skf.n_folds if acc_tmp > 0.51 else 0)
+
+                    if str.lower(opt_criterion) == 'map':
+                        Rval[k,i,j] += ap_tmp/skf.n_folds if acc_tmp > 0.50 else 0
+                    else: # 'acc' or other criterion
+                        Rval[k,i,j] += acc_tmp/skf.n_folds
 
     # print p, np.mean(p)
 
@@ -337,8 +352,10 @@ def kernel_fusion_classification(input_kernels_tr, input_kernels_te, a, feat_typ
                 kernels_te[feat_t] = a_best[2]*kernels_te[feat_t]['root'] + (1-a_best[2])*kernels_te[feat_t]['nodes']
             else:
                 for i in xrange(len(kernels_tr[feat_t]['root'])):
-                    kernels_tr[feat_t]['root'][i], pr  = utils.normalization(kernels_tr[feat_t]['root'][i][0])
-                    kernels_te[feat_t]['root'][i]      = pr * kernels_te[feat_t]['root'][i][0]
+                    kernels_tr[feat_t]['root'][i], pr  = utils.normalization(kernels_tr[feat_t]['root'][i][0] if np.sum(kernels_tr[feat_t]['root'][i][0]) > 0
+                                                                             else kernels_tr[feat_t]['nodes'][i][0])
+                    kernels_te[feat_t]['root'][i]      = pr * (kernels_te[feat_t]['root'][i][0] if np.sum(kernels_tr[feat_t]['root'][i][0]) > 0
+                                                               else kernels_te[feat_t]['nodes'][i][0])
 
                     xn_tr, xn_te = kernels_tr[feat_t]['nodes'][i], kernels_te[feat_t]['nodes'][i]
                     kernels_tr[feat_t]['nodes'][i], pn = utils.normalization(a_best[1]*xn_tr[0]+(1-a_best[1])*xn_tr[1] if len(xn_tr)==2 else xn_tr[0])
@@ -346,22 +363,6 @@ def kernel_fusion_classification(input_kernels_tr, input_kernels_te, a, feat_typ
 
                 kernels_tr[feat_t] = list(a_best[2]*np.array(kernels_tr[feat_t]['root']) + (1-a_best[2])*np.array(kernels_tr[feat_t]['nodes']))
                 kernels_te[feat_t] = list(a_best[2]*np.array(kernels_te[feat_t]['root']) + (1-a_best[2])*np.array(kernels_te[feat_t]['nodes']))
-
-        # for feat_t in kernels_tr.keys():
-        #     for tree_t in kernels_tr[feat_t].keys():
-        #         kernels_tr[feat_t][tree_t] = list(kernels_tr[feat_t][tree_t])
-        #         kernels_te[feat_t][tree_t] = list(kernels_te[feat_t][tree_t])
-        #         for i,x in enumerate(kernels_tr[feat_t][tree_t]):
-        #             x_tr = a_best[1]*x[0]+(1-a_best[1])*x[1] if (isinstance(x,tuple) and len(x)>1) else 2*x[0]
-        #             p = utils.argnormalize(x_tr)
-        #             kernels_tr[feat_t][tree_t][i] = p*x_tr
-        #
-        #             x = kernels_te[feat_t][tree_t][i]
-        #             x_te = a_best[1]*x[0]+(1-a_best[1])*x[1] if (isinstance(x,tuple) and len(x)>1) else 2*x[0]
-        #             kernels_te[feat_t][tree_t][i] = p*x_te
-        #
-        #     kernels_tr[feat_t] = list(a_best[2]*np.array(kernels_tr[feat_t]['root']) + (1-a_best[2])*np.array(kernels_tr[feat_t]['nodes']))
-        #     kernels_te[feat_t] = list(a_best[2]*np.array(kernels_te[feat_t]['root']) + (1-a_best[2])*np.array(kernels_te[feat_t]['nodes']))
 
         K_tr = K_te = None
         # Weight each channel accordingly
@@ -372,7 +373,8 @@ def kernel_fusion_classification(input_kernels_tr, input_kernels_te, a, feat_typ
             K_tr += a_best[3][j] * utils.sum_of_arrays(kernels_tr[feat_t], a_best[0])
             K_te += a_best[3][j] * utils.sum_of_arrays(kernels_te[feat_t], a_best[0])
 
-        # K_tr, K_te = np.sqrt(K_tr), np.sqrt(K_te)
+        if square_kernels:
+            K_tr, K_te = np.sqrt(K_tr), np.sqrt(K_te)
         acc, ap = _train_and_classify_binary(K_tr, K_te[te_msk], class_labels[tr_inds,k], class_labels[te_inds,k][te_msk], c=c_best)
 
         acc_classes.append(acc)

kernels.py
@@ -397,16 +397,17 @@ def intersection_kernel(X, Y=None, n_channels=1, nt=-1, verbose=True):
 
     shuffle(points)  # so all threads have similar workload
 
-    # DEBUG
-    # ---
-    # step = len(points)
-    # ret = _intersection_kernel(X, Y, points, n_channels=n_channels, tid=-1, verbose=True)
-    # ---
     step = np.int(np.floor(len(points)/nt)+1)
-    # ret = Parallel(n_jobs=nt, backend='threading')(delayed(_intersection_kernel)(X, Y, points[i*step:((i+1)*step if (i+1)*step < len(points) else len(points))],
-    #                                                                              n_channels=n_channels, tid=i, verbose=True)
+    # ---
+    # ret = Parallel(n_jobs=nt, backend='threading')(delayed(_intersection_kernel_batch)(X, Y, points[i*step:((i+1)*step if (i+1)*step < len(points) else len(points))],
+    #                                                                              n_channels=n_channels, job_id=i, verbose=True)
     #                                                for i in xrange(nt))
-
+    # # aggregate results of parallel computations
+    # Kr, Ke = ret[0][0], ret[0][1]
+    # for r in ret[1:]:
+    #     Kr += r[0]
+    #     Ke += r[1]
+    # ---
     ret = Parallel(n_jobs=nt, backend='threading')(delayed(_intersection_kernel)(X['root'][i], Y['root'][j], X['nodes'][i], Y['nodes'][j],
                                                                              n_channels=n_channels, job_id=job_id, verbose=True)
                                                for job_id,(i,j) in enumerate(points))
@@ -418,6 +419,7 @@ def intersection_kernel(X, Y=None, n_channels=1, nt=-1, verbose=True):
         i,j = points[job_id]
         for c in xrange(n_channels):
             Kr[c,i,j], Ke[c,i,j] = res[c,0], res[c,1]
+    # ---
 
     # if symmetric, replicate upper to lower triangle matrix
     if is_symmetric:
@@ -468,7 +470,7 @@ def _intersection_kernel(input_path, videonames, X, Y, points, tid=None, verbose
 
     return Kr, Kn
 
-def _intersection_kernel(X, Y, points, n_channels=1, tid=None, verbose=True):
+def _intersection_kernel_batch(X, Y, points, n_channels=1, job_id=None, verbose=True):
     """
     Compute the ATEP kernel.
     :param X:
@@ -490,7 +492,7 @@ def _intersection_kernel(X, Y, points, n_channels=1, tid=None, verbose=True):
 
     for pid,(i,j) in enumerate(points):
         if verbose:
-            print('[Parallel intersection kernel] Thread %d, progress = %.1f%%]' % (tid,100.*(pid+1)/len(points)))
+            print('[Parallel intersection kernel] Thread %d, progress = %.1f%%]' % (job_id,100.*(pid+1)/len(points)))
 
         for k in xrange(n_channels):
             Kr[k,i,j] = np.minimum(X['root'][i][k], Y['root'][j][k]).sum()  # * p

tracklet_clustering.py
@@ -37,10 +37,10 @@ def cluster_multithread(tracklets_path, videonames, clusters_path, nt=4):
     # inds = np.random.permutation(len(videonames))
     inds = np.linspace(0,len(videonames)-1,len(videonames)).astype('int')
     # step = np.int(np.floor(len(inds)/nt)+1)
-    Parallel(n_jobs=nt, backend='multiprocessing')(delayed(_cluster)(tracklets_path, videonames, \
+    Parallel(n_jobs=nt, backend='threading')(delayed(_cluster)(tracklets_path, videonames, \
                                                                [i], \
                                                                clusters_path, visualize=False)
-                                             for i in inds)
+                                                     for i in inds)
 
 
 def _cluster(tracklets_path, videonames, indices, clusters_path, visualize=False):

tracklet_extraction.py
@@ -46,7 +46,7 @@ def extract_multithread(fullvideonames, videonames, feat_types, tracklets_path, 
     inds = np.linspace(0,len(videonames)-1,len(videonames)).astype('int')
     # step = np.int(np.floor(len(inds)/nt)+1)
     #inds[i*step:((i+1)*step if (i+1)*step < len(inds) else len(inds))],
-    Parallel(n_jobs=nt, backend='multiprocessing')(delayed(_extract)(fullvideonames, videonames, \
+    Parallel(n_jobs=nt, backend='threading')(delayed(_extract)(fullvideonames, videonames, \
                                                                [i], \
                                                                feat_types, tracklets_path)
                                              for i in inds)

tracklet_representation.py
@@ -31,35 +31,35 @@ INTERNAL_PARAMETERS = dict(
 
 
 def compute_bovw_descriptors(tracklets_path, intermediates_path, videonames, traintest_parts, feat_types, feats_path, \
-                             pca_reduction=True, treelike=True, clusters_path=None):
+                             pca_reduction=False, treelike=True, clusters_path=None):
     _compute_bovw_descriptors(tracklets_path, intermediates_path, videonames, traintest_parts, np.arange(len(videonames)), feat_types, feats_path, \
                               pca_reduction=pca_reduction, treelike=treelike, clusters_path=clusters_path)
 
 def compute_fv_descriptors(tracklets_path, intermediates_path, videonames, traintest_parts, feat_types, feats_path, \
-                           pca_reduction=True, treelike=True, clusters_path=None):
+                           pca_reduction=False, treelike=True, clusters_path=None):
     _compute_fv_descriptors(tracklets_path, intermediates_path, videonames, traintest_parts, np.arange(len(videonames)), feat_types, feats_path, \
                             pca_reduction=pca_reduction, treelike=treelike, clusters_path=clusters_path)
 
 def compute_vd_descriptors(tracklets_path, intermediates_path, videonames, traintest_parts, feat_types, feats_path, \
-                           pca_reduction=True, treelike=True, clusters_path=None):
+                           pca_reduction=False, treelike=True, clusters_path=None):
     _compute_vd_descriptors(tracklets_path, intermediates_path, videonames, traintest_parts, np.arange(len(videonames)), feat_types, feats_path, \
                             pca_reduction=pca_reduction, treelike=treelike, clusters_path=clusters_path)
 
 
 def compute_bovw_descriptors_multiprocess(tracklets_path, intermediates_path, videonames, traintest_parts, st, num_videos, feat_types, feats_path, \
-                                          pca_reduction=True, treelike=True, clusters_path=None):
+                                          pca_reduction=False, treelike=True, clusters_path=None):
     inds = np.linspace(st, st+num_videos-1, num_videos)
     _compute_bovw_descriptors(tracklets_path, intermediates_path, videonames, traintest_parts, inds, feat_types, feats_path, \
                               pca_reduction=pca_reduction, treelike=treelike, clusters_path=clusters_path)
 
 def compute_fv_descriptors_multiprocess(tracklets_path, intermediates_path, videonames, traintest_parts, st, num_videos, feat_types, feats_path, \
-                                        pca_reduction=True, treelike=True, clusters_path=None):
+                                        pca_reduction=False, treelike=True, clusters_path=None):
     inds = np.linspace(st, st+num_videos-1, num_videos)
     _compute_fv_descriptors(tracklets_path, intermediates_path, videonames, traintest_parts, inds, feat_types, feats_path, \
                             pca_reduction=pca_reduction, treelike=treelike, clusters_path=clusters_path)
 
 def compute_vd_descriptors_multiprocess(tracklets_path, intermediates_path, videonames, traintest_parts, st, num_videos, feat_types, feats_path, \
-                                        pca_reduction=True, treelike=True, clusters_path=None):
+                                        pca_reduction=False, treelike=True, clusters_path=None):
     inds = np.linspace(st, st+num_videos-1, num_videos)
     _compute_vd_descriptors(tracklets_path, intermediates_path, videonames, traintest_parts, inds, feat_types, feats_path, \
                             pca_reduction=pca_reduction, treelike=treelike, clusters_path=clusters_path)
@@ -67,21 +67,21 @@ def compute_vd_descriptors_multiprocess(tracklets_path, intermediates_path, vide
 
 def compute_bovw_descriptors_multithread(tracklets_path, intermediates_path, videonames, traintest_parts, feat_types, feats_path, \
                                          nt=4, pca_reduction=False, treelike=True, clusters_path=None):
-    Parallel(n_jobs=nt, backend='multiprocessing')(delayed(_compute_bovw_descriptors)(tracklets_path, intermediates_path, videonames, traintest_parts, \
+    Parallel(n_jobs=nt, backend='threading')(delayed(_compute_bovw_descriptors)(tracklets_path, intermediates_path, videonames, traintest_parts, \
                                                                                 [i], feat_types, feats_path, \
                                                                                 pca_reduction=pca_reduction, treelike=treelike, clusters_path=clusters_path)
                                                            for i in xrange(len(videonames)))
 
 def compute_fv_descriptors_multithread(tracklets_path, intermediates_path, videonames, traintest_parts, feat_types, feats_path, \
                                        nt=4, pca_reduction=False, treelike=True, clusters_path=None):
-    Parallel(n_jobs=nt, backend='multiprocessing')(delayed(_compute_fv_descriptors)(tracklets_path, intermediates_path, videonames, traintest_parts, \
+    Parallel(n_jobs=nt, backend='threading')(delayed(_compute_fv_descriptors)(tracklets_path, intermediates_path, videonames, traintest_parts, \
                                                                               [i], feat_types, feats_path, \
                                                                               pca_reduction=pca_reduction, treelike=treelike, clusters_path=clusters_path)
                                                            for i in xrange(len(videonames)))
 
 def compute_vd_descriptors_multithread(tracklets_path, intermediates_path, videonames, traintest_parts, feat_types, feats_path, \
                                        nt=4, pca_reduction=False, treelike=True, clusters_path=None):
-    Parallel(n_jobs=nt, backend='multiprocessing')(delayed(_compute_vd_descriptors)(tracklets_path, intermediates_path, videonames, traintest_parts, \
+    Parallel(n_jobs=nt, backend='threading')(delayed(_compute_vd_descriptors)(tracklets_path, intermediates_path, videonames, traintest_parts, \
                                                                               [i], feat_types, feats_path, \
                                                                               pca_reduction=pca_reduction, treelike=treelike, clusters_path=clusters_path)
                                                            for i in xrange(len(videonames)))
@@ -93,7 +93,7 @@ def compute_vd_descriptors_multithread(tracklets_path, intermediates_path, video
 
 
 def _compute_bovw_descriptors(tracklets_path, intermediates_path, videonames, traintest_parts, indices, feat_types, feats_path, \
-                              pca_reduction=True, treelike=True, clusters_path=None):
+                              pca_reduction=False, treelike=True, clusters_path=None):
     try:
         makedirs(feats_path)
     except OSError:
@@ -263,7 +263,7 @@ def _compute_fv_descriptors(tracklets_path, intermediates_path, videonames, trai
 
 
 def _compute_vd_descriptors(tracklets_path, intermediates_path, videonames, traintest_parts, indices, feat_types, feats_path, \
-                            pca_reduction=True, treelike=True, clusters_path=None):
+                            pca_reduction=False, treelike=True, clusters_path=None):
     try:
         makedirs(feats_path)
     except OSError:
@@ -368,7 +368,7 @@ def _compute_vd_descriptors(tracklets_path, intermediates_path, videonames, trai
             print('%s -> DONE (in %.2f secs)' % (videonames[i], elapsed_time))
 
 
-def train_bovw_codebooks(tracklets_path, videonames, traintest_parts, feat_types, intermediates_path, pca_reduction=True, nt=1):
+def train_bovw_codebooks(tracklets_path, videonames, traintest_parts, feat_types, intermediates_path, pca_reduction=False, nt=1):
     try:
         makedirs(intermediates_path)
     except OSError:
@@ -388,32 +388,7 @@ def train_bovw_codebooks(tracklets_path, videonames, traintest_parts, feat_types
 
             start_time = time.time()
 
-            D = None  # feat_t's sampled tracklets
-            ptr = 0
-            for j in range(0, total):
-                idx = train_inds[j]
-
-                filepath = tracklets_path + feat_t + '/' + videonames[idx] + '.pkl'
-                if not isfile(filepath):
-                    sys.stderr.write('# ERROR: missing training instance'
-                                     ' {}\n'.format(filepath))
-                    sys.stderr.flush()
-                    quit()
-
-                with open(filepath, 'rb') as f:
-                    d = cPickle.load(f)
-
-                # init sample
-                if D is None:
-                    D = np.zeros((INTERNAL_PARAMETERS['n_samples'], d.shape[1]), dtype=np.float32)
-                # create a random permutation for sampling some tracklets in this vids
-                randp = np.random.permutation(d.shape[0])
-                if d.shape[0] > num_samples_per_vid:
-                    randp = randp[:num_samples_per_vid]
-                D[ptr:ptr+len(randp),:] = d[randp,:]
-                ptr += len(randp)
-            D = D[:ptr,:]  # cut out extra reserved space
-
+            D = load_tracklets_sample(tracklets_path, videonames, train_inds, feat_t, num_samples_per_vid)
 
             # (special case) trajectory features are originally positions
             if feat_t == 'trj':
@@ -429,7 +404,7 @@ def train_bovw_codebooks(tracklets_path, videonames, traintest_parts, feat_types
             # train codebook for later BOVW computation
             D = np.ascontiguousarray(D, dtype=np.float32)
             cb = ynumpy.kmeans(D, INTERNAL_PARAMETERS['bovw_codebook_k'], \
-                               distance_type=2, nt=nt, niter=20, seed=0, redo=1, \
+                               distance_type=2, nt=nt, niter=100, seed=0, redo=1, \
                                verbose=True, normalize=False, init='kmeans++')
 
             with open(output_filepath, 'wb') as f:
@@ -447,67 +422,102 @@ def train_fv_gmms(tracklets_path, videonames, traintest_parts, feat_types, inter
 
     for k, part in enumerate(traintest_parts):
         train_inds = np.where(part <= 0)[0]  # train codebook for each possible training parition
-        total = len(train_inds)
-        num_samples_per_vid = int(INTERNAL_PARAMETERS['n_samples'] / float(total))
+        num_samples_per_vid = int(INTERNAL_PARAMETERS['n_samples'] / float(len(train_inds)))
 
         # process the videos
         for i, feat_t in enumerate(feat_types):
+            D = None
+
+            # Train GMMs
             output_filepath = intermediates_path + 'gmm' + ('_pca-' if pca_reduction else '-') + feat_t + '-' + str(k) + '.pkl'
             if isfile(output_filepath):
                 print('%s -> OK' % output_filepath)
-                continue
+            else:
+                start_time = time.time()
 
-            start_time = time.time()
+                D = load_tracklets_sample(tracklets_path, videonames, train_inds, feat_t, num_samples_per_vid)
 
-            D = None  # feat_t's sampled tracklets
-            ptr = 0
-            for j in range(0, total):
-                print j, total
-                idx = train_inds[j]
+                # (special case) trajectory features are originally positions
+                if feat_t == 'trj':
+                    D = convert_positions_to_displacements(D)
 
-                filepath = tracklets_path + feat_t + '/' + videonames[idx] + '.pkl'
-                if not isfile(filepath):
-                    sys.stderr.write('# ERROR: missing training instance'
-                                     ' {}\n'.format(filepath))
-                    sys.stderr.flush()
-                    quit()
+                # scale (rootSIFT)
+                D = rootSIFT(preprocessing.normalize(D, norm='l1', axis=1))
 
-                with open(filepath, 'rb') as f:
-                    d = cPickle.load(f)
+                # compute PCA map and reduce dimensionality
+                if pca_reduction:
+                    pca = PCA(n_components=int(INTERNAL_PARAMETERS['reduction_factor']*D.shape[1]), copy=False)
+                    D = pca.fit_transform(D)
 
-                # init sample
-                if D is None:
-                    D = np.zeros((INTERNAL_PARAMETERS['n_samples'], d.shape[1]), dtype=np.float32)
-                # create a random permutation for sampling some tracklets in this vids
-                randp = np.random.permutation(d.shape[0])
-                if d.shape[0] > num_samples_per_vid:
-                    randp = randp[:num_samples_per_vid]
-                D[ptr:ptr+len(randp),:] = d[randp,:]
-                ptr += len(randp)
-            D = D[:ptr,:]  # cut out extra reserved space
+                # train GMMs for later FV computation
+                D = np.ascontiguousarray(D, dtype=np.float32)
+                gmm = ynumpy.gmm_learn(D, INTERNAL_PARAMETERS['fv_gmm_k'], nt=nt, niter=250, redo=1)
+
+                with open(output_filepath, 'wb') as f:
+                    cPickle.dump(dict(pca=(pca if pca_reduction else None), gmm=gmm), f)
+                with open(intermediates_path + 'gmm-sample' + ('_pca-' if pca_reduction else '-') + feat_t + '-' + str(k) + '.pkl', 'wb') as f:
+                    cPickle.dump(D,f)
+
+                elapsed_time = time.time() - start_time
+                print('%s -> DONE (in %.2f secs)' % (feat_t, elapsed_time))
+
+            # Dimensionality reduction
+            # gmm_model_filepath = output_filepath
+            # output_filepath = intermediates_path + 'gmm-' + ('_pca-' if pca_reduction else '-') + feat_t + '-' + str(k) + '.pkl'
+            # if isfile(output_filepath):
+            #     print('%s -> OK' % output_filepath)
+            # else:
+            #     with open(gmm_model_filepath, 'rb') as f:
+            #         gmm_model = cPickle.load(f)['gmm']
+            #
+            #     if D is None:
+            #         try:
+            #             with open(intermediates_path + 'gmm-sample' + ('_pca-' if pca_reduction else '-') + feat_t + '-' + str(k) + '.pkl', 'rb') as f:
+            #                 D = cPickle.load(f)
+            #         except:
+            #             V = [None] * len(train_inds)
+            #             for j,idx in enumerate(train_inds):
+            #                 print("[train_fv_gmms] PCA sample generation: %d out of %d\n" % (j, len(train_inds)))
+            #                 d = None
+            #                 try:
+            #                     with open(tracklets_path + feat_t + '/' + videonames[idx] + '.pkl', 'rb') as f:
+            #                         d = cPickle.load(f)
+            #                 except IOError:
+            #                     continue
+            #                 V[j] = ynumpy.fisher(gmm_model, np.ascontiguousarray(d, dtype=np.float32), include=INTERNAL_PARAMETERS['fv_repr_feats'])
+            #
+            #             with open(intermediates_path + 'gmm-sample' + ('_pca-' if pca_reduction else '-') + feat_t + '-' + str(k) + '.pkl', 'wb') as f:
+            #                 cPickle.dump(V,f)
 
 
-            # (special case) trajectory features are originally positions
-            if feat_t == 'trj':
-                D = convert_positions_to_displacements(D)
+def load_tracklets_sample(tracklets_path, videonames, data_inds, feat_t, num_samples_per_vid):
+    D = None  # feat_t's sampled tracklets
+    ptr = 0
+    for j in range(0, len(data_inds)):
+        idx = data_inds[j]
 
-            # scale (rootSIFT)
-            D = rootSIFT(preprocessing.normalize(D, norm='l1', axis=1))
+        filepath = tracklets_path + feat_t + '/' + videonames[idx] + '.pkl'
+        if not isfile(filepath):
+            sys.stderr.write('# ERROR: missing training instance'
+                             ' {}\n'.format(filepath))
+            sys.stderr.flush()
+            quit()
 
-            # compute PCA map and reduce dimensionality
-            if pca_reduction:
-                pca = PCA(n_components=int(INTERNAL_PARAMETERS['reduction_factor']*D.shape[1]), copy=False)
-                D = pca.fit_transform(D)
+        with open(filepath, 'rb') as f:
+            d = cPickle.load(f)
 
-            # train GMMs for later FV computation
-            D = np.ascontiguousarray(D, dtype=np.float32)
-            gmm = ynumpy.gmm_learn(D, INTERNAL_PARAMETERS['fv_gmm_k'], nt=nt, niter=500, redo=1)
+        # init sample
+        if D is None:
+            D = np.zeros((INTERNAL_PARAMETERS['n_samples'], d.shape[1]), dtype=np.float32)
+        # create a random permutation for sampling some tracklets in this vids
+        randp = np.random.permutation(d.shape[0])
+        if d.shape[0] > num_samples_per_vid:
+            randp = randp[:num_samples_per_vid]
+        D[ptr:ptr+len(randp),:] = d[randp,:]
+        ptr += len(randp)
 
-            with open(output_filepath, 'wb') as f:
-                cPickle.dump(dict(pca=(pca if pca_reduction else None), gmm=gmm), f)
+    return D[:ptr,:] # cut out extra reserved space
 
-            elapsed_time = time.time() - start_time
-            print('%s -> DONE (in %.2f secs)' % (feat_t, elapsed_time))
 
 
 # ==============================================================================
@@ -582,7 +592,7 @@ def reconstruct_tree_from_leafs(leafs):
 
     return h
 
-def bovw(codebook, X):
+def bovw(codebook, X, nt=1):
     inds, dists = ynumpy.knn(X, codebook, nnn=1, distance_type=2, nt=1)
     bins, _ = np.histogram(inds[:,0], bins=INTERNAL_PARAMETERS['bovw_codebook_k'])
 