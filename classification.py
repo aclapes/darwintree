major changes, these including: edges dont go through disk / bugfix: removed the normalization when extractin bovw feats / better console output when computing kernels / added comentaries

classification.py
@@ -19,7 +19,7 @@ INTERNAL_PARAMETERS = dict(
 
 svm_parameters = {'C': [1e-4,1e-3,1e-2,1e-1,1,10,100,1e3,1e4], \
                   'gamma' : [1e-7, 1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1, 10, 100, 1e3, 1e4], \
-                  'kernel':['rbf'], 'class_weight':['balanced']}
+                  'kernel':['linear'], 'class_weight':['balanced']}
 
 rbf_parameters = {'n_estimators': [10,20,30,40],
                   'max_features': ['auto','sqrt','log2',0.5,0.3],
@@ -224,16 +224,18 @@ def classify(input_kernels, class_labels, traintest_parts, params, feat_types, s
 
 
 def kernel_fusion_classification(input_kernels_tr, input_kernels_te, a, feat_types, class_labels, train_test_idx, \
-                                 C=[1], square_kernels=True, opt_criterion='acc'):
+                                 C=[1], square_kernels=False, opt_criterion='acc'):
     '''
 
-    :param kernels_tr:
-    :param kernels_te:
-    :param a: trade-off parameter controlling importance of root representation vs edges representation
+    :param input_kernels_tr:
+    :param input_kernels_te:
+    :param a:
     :param feat_types:
     :param class_labels:
     :param train_test_idx:
-    :param c:
+    :param C:
+    :param square_kernels:
+    :param opt_criterion:
     :return:
     '''
 
@@ -246,7 +248,7 @@ def kernel_fusion_classification(input_kernels_tr, input_kernels_te, a, feat_typ
     # lb = LabelBinarizer(neg_label=-1, pos_label=1)
 
     class_ints = np.dot(class_labels, np.logspace(0, class_labels.shape[1]-1, class_labels.shape[1]))
-    skf = cross_validation.StratifiedKFold(class_ints[tr_inds], n_folds=2, shuffle=False, random_state=74)
+    skf = cross_validation.StratifiedKFold(class_ints[tr_inds], n_folds=4, shuffle=True, random_state=74)
 
     Rval = np.zeros((class_labels.shape[1], len(a), len(C)), dtype=np.float32)
     for k in xrange(class_labels.shape[1]):
@@ -266,8 +268,8 @@ def kernel_fusion_classification(input_kernels_tr, input_kernels_te, a, feat_typ
                 else:
                     kernels_tr[feat_t]['root']  = [utils.normalize(x[0] if np.sum(x[0])>0 else kernels_tr[feat_t]['nodes'][j][0])
                                                    for j,x in enumerate(kernels_tr[feat_t]['root'])]
-                    kernels_tr[feat_t]['nodes'] = [utils.normalize(a_i[1]*x[0]+(1-a_i[1])*x[1] if len(x)==2 else x[0])
-                                                   for x in kernels_tr[feat_t]['nodes']]
+                    kernels_tr[feat_t]['nodes'] = [utils.normalize(a_i[1][j]*x[0]+(1-a_i[1][j])*x[1] if len(x)==2 else x[0])
+                                                   for j,x in enumerate(kernels_tr[feat_t]['nodes'])]
 
                     kernels_tr[feat_t] = list(a_i[2]*np.array(kernels_tr[feat_t]['root']) + (1-a_i[2])*np.array(kernels_tr[feat_t]['nodes']))
 
@@ -294,10 +296,10 @@ def kernel_fusion_classification(input_kernels_tr, input_kernels_te, a, feat_typ
                     acc_tmp, ap_tmp, _ = _train_and_classify_binary(
                         K_tr[val_tr_inds,:][:,val_tr_inds], K_tr[val_te_msk,:][:,val_tr_inds], \
                         class_labels[tr_inds,k][val_tr_inds], class_labels[tr_inds,k][val_te_msk], \
-                        probability=False, c=c_j)
+                        probability=True, c=c_j)
 
                     if str.lower(opt_criterion) == 'map':
-                        Rval[k,i,j] += ap_tmp/skf.n_folds  # if acc_tmp > 0.50 else 0
+                        Rval[k,i,j] += ap_tmp/skf.n_folds # if acc_tmp > 0.50 else 0
                     else: # 'acc' or other criterion
                         Rval[k,i,j] += acc_tmp/skf.n_folds
 
@@ -357,8 +359,8 @@ def kernel_fusion_classification(input_kernels_tr, input_kernels_te, a, feat_typ
                                                                else kernels_te[feat_t]['nodes'][i][0])
 
                     xn_tr, xn_te = kernels_tr[feat_t]['nodes'][i], kernels_te[feat_t]['nodes'][i]
-                    kernels_tr[feat_t]['nodes'][i], pn = utils.normalization(a_best[1]*xn_tr[0]+(1-a_best[1])*xn_tr[1] if len(xn_tr)==2 else xn_tr[0])
-                    kernels_te[feat_t]['nodes'][i]     = pn * (a_best[1]*xn_te[0]+(1-a_best[1])*xn_te[1] if len(xn_te)==2 else xn_te[0])
+                    kernels_tr[feat_t]['nodes'][i], pn = utils.normalization(a_best[1][i]*xn_tr[0]+(1-a_best[1][i])*xn_tr[1] if len(xn_tr)==2 else xn_tr[0])
+                    kernels_te[feat_t]['nodes'][i]     = pn * (a_best[1][i]*xn_te[0]+(1-a_best[1][i])*xn_te[1] if len(xn_te)==2 else xn_te[0])
 
                 kernels_tr[feat_t] = list(a_best[2]*np.array(kernels_tr[feat_t]['root']) + (1-a_best[2])*np.array(kernels_tr[feat_t]['nodes']))
                 kernels_te[feat_t] = list(a_best[2]*np.array(kernels_te[feat_t]['root']) + (1-a_best[2])*np.array(kernels_te[feat_t]['nodes']))
@@ -374,7 +376,7 @@ def kernel_fusion_classification(input_kernels_tr, input_kernels_te, a, feat_typ
 
         if square_kernels:
             K_tr, K_te = np.sign(K_tr) * np.sqrt(np.abs(K_tr)), np.sign(K_te) * np.sqrt(np.abs(K_te)) #np.sqrt(K_tr), np.sqrt(K_te)
-        acc, ap, _ = _train_and_classify_binary(K_tr, K_te[te_msk], class_labels[tr_inds,k], class_labels[te_inds,k][te_msk], c=c_best)
+        acc, ap, _ = _train_and_classify_binary(K_tr, K_te[te_msk], class_labels[tr_inds,k], class_labels[te_inds,k][te_msk], probability=True, c=c_best)
 
         acc_classes.append(acc)
         ap_classes.append(ap)
@@ -505,7 +507,8 @@ def simple_voting_classification(input_kernels_tr, input_kernels_te, a, feat_typ
 
     return dict(acc_classes=acc_classes, ap_classes=ap_classes)
 
-def learning_based_fusion_classification(input_kernels_tr, input_kernels_te, a, feat_types, class_labels, train_test_idx, C=[1], opt_criterion='acc'):
+def learning_based_fusion_classification(input_kernels_tr, input_kernels_te, a, feat_types, class_labels, train_test_idx, \
+                                         C=[1], square_kernel=False, opt_criterion='acc'):
     '''
 
     :param kernels_tr:
@@ -522,7 +525,7 @@ def learning_based_fusion_classification(input_kernels_tr, input_kernels_te, a, 
     # lb = LabelBinarizer(neg_label=-1, pos_label=1)
 
     class_ints = np.dot(class_labels, np.logspace(0, class_labels.shape[1]-1, class_labels.shape[1]))
-    skf = cross_validation.StratifiedKFold(class_ints[tr_inds], n_folds=4, shuffle=False, random_state=74)
+    skf = cross_validation.StratifiedKFold(class_ints[tr_inds], n_folds=10, shuffle=False, random_state=74)
 
     # S = [None] * class_labels.shape[1]  # selected (best) params
     # p = [None] * class_labels.shape[1]  # performances
@@ -548,7 +551,10 @@ def learning_based_fusion_classification(input_kernels_tr, input_kernels_te, a, 
 
         for i, a_i in enumerate(a):
             for k, x in enumerate(kernels_tr):
-                K_tr = utils.normalize(a_i*x[0]+(1-a_i)*x[1]) if isinstance(x,tuple) else utils.normalize(x)
+                K_tr = utils.normalize(a_i*x[0]+(1-a_i)*x[1]) if len(x)==2 else utils.normalize(x[0])
+
+                if square_kernel:
+                    K_tr = np.sign(K_tr) * np.sqrt(np.abs(K_tr))
 
                 for j, c_j in enumerate(C):
                     for (val_tr_inds, _) in skf:
@@ -558,16 +564,16 @@ def learning_based_fusion_classification(input_kernels_tr, input_kernels_te, a, 
                         negatives_msk = np.all(class_labels[tr_inds] <= 0, axis=1)
                         val_te_msk[negatives_msk] = False
 
-                        acc, ap = _train_and_classify_binary(
+                        acc, ap, _ = _train_and_classify_binary(
                             K_tr[val_tr_inds,:][:,val_tr_inds], K_tr[val_te_msk,:][:,val_tr_inds], \
                             class_labels[tr_inds,cl][val_tr_inds], class_labels[tr_inds,cl][val_te_msk], \
-                            probability=False, c=c_j)
+                            probability=True, c=c_j)
 
                         # TODO: decide what it is
                         if str.lower(opt_criterion) == 'map':
-                            Rp[cl][k,i,j] += acc / skf.n_folds
+                            Rp[cl][k,i,j] += ap / skf.n_folds
                         else:
-                            Rp[cl][k,i,j] += ap/skf.n_folds if acc > 0.5 else 0
+                            Rp[cl][k,i,j] += acc / skf.n_folds
 
     params = [None] * class_labels.shape[1]
     perfs = [None] * class_labels.shape[1]
@@ -594,41 +600,43 @@ def learning_based_fusion_classification(input_kernels_tr, input_kernels_te, a, 
             # val_te_msk[negatives_msk] = False
 
             # Get the predictions of each and every kernel
-            # X = np.zeros((len(val_te_inds), params[cl].shape[0]))
-            X = np.zeros((len(val_te_inds), 2*params[cl].shape[0]))  # matrix of predictions
+            X = np.zeros((len(val_te_inds), params[cl].shape[0]))
+            # X = np.zeros((len(val_te_inds), 2*params[cl].shape[0]))  # matrix of predictions
             for k,x in enumerate(kernels_tr):
                 # Value of best parameters
                 a_val, c_val = params[cl][k,0], params[cl][k,1]
 
                 # Merge a kernel according to a_val
-                K_tr_aux = (a_val*x[0]+(1-a_val)*x[1]) if isinstance(x,tuple) else x
-                K_tr = utils.normalize(K_tr_aux)
+                K_tr = utils.normalize(a_val*x[0]+(1-a_val)*x[1] if len(x)==2 else x[0])
+
+                if square_kernel:
+                    K_tr = np.sign(K_tr) * np.sqrt(np.abs(K_tr))
 
                 # Train using c_val as SVM-C parameter
-                clf = _train_binary(K_tr[val_tr_inds,:][:,val_tr_inds], class_labels[tr_inds,cl][val_tr_inds], c=c_val)
-                # X[:,k] = clf.decision_function(K_tr[val_te_inds,:][:,val_tr_inds])
-                X[:,2*k] = clf.predict_proba(K_tr[val_te_inds,:][:,val_tr_inds])[:,0]
-                X[:,2*k+1] = clf.decision_function(K_tr[val_te_inds,:][:,val_tr_inds])
+                clf = _train_binary(K_tr[val_tr_inds,:][:,val_tr_inds], class_labels[tr_inds,cl][val_tr_inds], probability=True, c=c_val)
+                X[:,k] = clf.decision_function(K_tr[val_te_inds,:][:,val_tr_inds])
+                # X[:,2*k] = clf.predict_proba(K_tr[val_te_inds,:][:,val_tr_inds])[:,0]
+                # X[:,2*k+1] = clf.decision_function(K_tr[val_te_inds,:][:,val_tr_inds])
             D_tr.append(X)
             y_tr.append(class_labels[tr_inds,cl][val_te_inds])
 
         D_tr = (np.vstack(D_tr))
-        # D_tr = np.sign(D_tr)*np.sqrt(np.abs(D_tr))
-        # D_tr = np.hstack([preprocessing.normalize(D_tr[:,::2], norm='l2'), preprocessing.normalize(D_tr[:,1::2], norm='l2')])
+
         y_tr = np.concatenate(y_tr)
         n = len(class_labels[tr_inds,cl])
-        # LOOCV = cross_validation.KFold(n, n_folds=n)
+
         if str.lower(opt_criterion) == 'map':
             grid_scorer = make_scorer(average_precision_score, greater_is_better=True)
         else:
             grid_scorer = make_scorer(average_binary_accuracy, greater_is_better=True)
-        LOOCV = cross_validation.StratifiedKFold(class_labels[tr_inds,cl], n_folds=3, shuffle=False, random_state=74)
+        LOOCV = cross_validation.StratifiedKFold(class_labels[tr_inds,cl], n_folds=2, shuffle=False, random_state=74)
         clfs[cl] = grid_search.GridSearchCV(svm.SVC(), svm_parameters, \
                                        n_jobs=20, cv=LOOCV, scoring=grid_scorer, verbose=False)
         clfs[cl].fit(D_tr,y_tr)
         clfs[cl].best_params_
 
     val_scores = [clf.best_score_ for clf in clfs]
+    print val_scores
     print np.mean(val_scores), np.std(val_scores)
 
     # quit()
@@ -645,8 +653,10 @@ def learning_based_fusion_classification(input_kernels_tr, input_kernels_te, a, 
 
         for k,x in enumerate(kernels_tr):
             a_val, c_val = params[cl][k,0],params[cl][k,1]
-            K_tr, F[cl,k] = utils.normalization((a_val*x[0]+(1-a_val)*x[1]) if isinstance(x,tuple) else x)
-            ind_clfs[cl][k] = _train_binary(K_tr, class_labels[tr_inds,cl], c=c_val)
+            K_tr, F[cl,k] = utils.normalization((a_val*x[0]+(1-a_val)*x[1]) if len(x)==2 else x[0])
+            if square_kernel:
+                K_tr = np.sign(K_tr) * np.sqrt(np.abs(K_tr))
+            ind_clfs[cl][k] = _train_binary(K_tr, class_labels[tr_inds,cl], probability=True, c=c_val)
 
     # Use a mask to exclude negatives from test
     te_msk = np.ones((len(te_inds),), dtype=np.bool)
@@ -657,24 +667,23 @@ def learning_based_fusion_classification(input_kernels_tr, input_kernels_te, a, 
     acc_classes = []
     ap_classes = []
     for cl in xrange(class_labels.shape[1]):
-        # X_te = np.zeros((len(te_inds), len(kernels_te)))
-        X_te = np.zeros((len(te_inds), 2*len(kernels_te)))
+        X_te = np.zeros((len(te_inds), len(kernels_te)))
+        # X_te = np.zeros((len(te_inds), 2*len(kernels_te)))
         for k,x in enumerate(kernels_te):
             a_val, c_val = params[cl][k,0], params[cl][k,1]
 
-            K_te = F[cl,k] * ((a_val*x[0]+(1-a_val)*x[1]) if isinstance(x,tuple) else x)
-            # K_te = (a_val*x[0]+(1-a_val)*x[1]) if isinstance(x,tuple) else x
-            # X_te[:,k] = ind_clfs[cl][k].decision_function(K_te)
-            X_te[:,2*k] = ind_clfs[cl][k].predict_proba(K_te)[:,0]
-            X_te[:,2*k+1] = ind_clfs[cl][k].decision_function(K_te)
+            K_te = F[cl,k] * ((a_val*x[0]+(1-a_val)*x[1]) if len(x)==2 else x[0])
+            if square_kernel:
+                K_te = np.sign(K_te) * np.sqrt(np.abs(K_te))
+            X_te[:,k] = ind_clfs[cl][k].decision_function(K_te)
+            # X_te[:,2*k] = ind_clfs[cl][k].predict_proba(K_te)[:,0]
+            # X_te[:,2*k+1] = ind_clfs[cl][k].decision_function(K_te)
 
         X_te = X_te[te_msk,:]
-        # X_te = np.sign(X_te)*np.sqrt(np.abs(X_te))
-        # X_te = np.hstack([preprocessing.normalize(X_te[te_msk,::2],norm='l2'), preprocessing.normalize(X_te[te_msk,1::2],norm='l2')])
 
         y_preds = clfs[cl].predict(X_te)
         acc = average_binary_accuracy(class_labels[te_inds,cl][te_msk], y_preds)
-        ap = average_precision_score(class_labels[te_inds,cl][te_msk], y_preds)
+        ap = average_precision_score(class_labels[te_inds,cl][te_msk], y_preds) #clfs[cl].decision_function(X_te))
 
         acc_classes.append(acc)
         ap_classes.append(ap)
@@ -863,7 +872,7 @@ def learning_based_fusion_classification2(input_kernels_tr, input_kernels_te, a,
 
 def _train_binary(K_tr, train_labels, probability=False, c=1.0):
     # Train
-    clf = svm.SVC(kernel='precomputed', class_weight='balanced', C=c, max_iter=-1, tol=1e-3, verbose=False)
+    clf = svm.SVC(kernel='precomputed', class_weight='balanced', C=c, max_iter=-1, tol=1e-7, probability=probability, verbose=False)
     clf.fit(K_tr, train_labels)
 
     return clf
@@ -872,17 +881,12 @@ def _train_binary(K_tr, train_labels, probability=False, c=1.0):
 def _train_and_classify_binary(K_tr, K_te, train_labels, test_labels, probability=False, c=1.0):
     clf = _train_binary(K_tr, train_labels, probability=probability, c=c)
 
-    # Predict
-    test_scores = clf.decision_function(K_te)
-    test_preds = clf.predict(K_te)
-
     # Compute accuracy and average precision
+    test_preds = clf.predict(K_te)
     acc = average_binary_accuracy(test_labels, test_preds)
 
-    # TODO: decide what is it
-    # ap = average_precision_score(test_labels, test_preds)
-    ap = average_precision_score(test_labels, test_scores, average='weighted')
-    # precision_recall_fscore_support
+    test_scores = clf.predict_proba(K_te)
+    ap = average_precision_score(test_labels, test_scores[:,1], average='weighted')
 
     return acc, ap, test_preds
 

tracklet_clustering.py
@@ -1,6 +1,6 @@
 __author__ = 'aclapes'
 
-from os.path import isfile, exists
+from os.path import isfile, exists, join
 from os import makedirs
 import cPickle
 import random
@@ -61,17 +61,20 @@ def _cluster(tracklets_path, videonames, indices, clusters_path, visualize=False
     # process the videos
     total = len(videonames)
     for i in indices:
-        if isfile(clusters_path + videonames[i] + '.pkl'):
+        if isfile(join(clusters_path, videonames[i] + '.pkl')):
             print('%s -> OK' % videonames[i])
             continue
 
+        try:
+            with open(join(tracklets_path, 'obj', videonames[i] + '.pkl'), 'rb') as f:
+                data_obj = cPickle.load(f)
+            with open(join(tracklets_path, 'trj', videonames[i] + '.pkl'), 'rb') as f:
+                data_trj = cPickle.load(f)
+        except IOError:
+            sys.stderr.write("[Error] Tracklet files not found for %s." % videonames[i])
+            continue
+
         start_time = time.time()
-
-        with open(tracklets_path + 'obj/' + videonames[i] + '.pkl', 'rb') as f:
-            data_obj = cPickle.load(f)
-        with open(tracklets_path + 'trj/' + videonames[i] + '.pkl', 'rb') as f:
-            data_trj = cPickle.load(f)
-
         # (Sec. 2.2) get a dictionary of separate channels
         D = dict()
         for k in xrange(data_obj.shape[0]): # range(0,100):  #
@@ -133,9 +136,9 @@ def _cluster(tracklets_path, videonames, indices, clusters_path, visualize=False
         tree = reconstruct_tree_from_leafs(np.unique(int_paths))
 
         elapsed_time = time.time() - start_time
-        print('%s -> %s (in %.2f secs)' % (clusters_path + videonames[i] + '.pkl', 'YES' if success else 'NO', elapsed_time))
+        print('%s -> %s (in %.2f secs)' % (join(clusters_path, videonames[i] + '.pkl'), 'YES' if success else 'NO', elapsed_time))
 
-        with open(clusters_path + videonames[i] + '.pkl', 'wb') as f:
+        with open(join(clusters_path, videonames[i] + '.pkl'), 'wb') as f:
             cPickle.dump({'best_labels' : best_labels, 'int_paths' : int_paths, 'tree' : tree, 'ridge' : ridge}, f)
 
         # DEBUG

tracklet_extraction.py
@@ -20,7 +20,7 @@ INTERNAL_PARAMETERS = dict(
     L = 15,
     feats_dict = dict(
         obj = 10,
-        trj = 2,
+        trj = 2,  # 2-by-L actually
         hog = 96,
         hof = 108,
         mbh = 192
@@ -65,19 +65,20 @@ def _extract(fullvideonames, videonames, indices, feat_types, tracklets_path):
     feats_beginend = get_features_beginend(INTERNAL_PARAMETERS['feats_dict'], INTERNAL_PARAMETERS['L'])
 
     # prepare output directories
-    try:
-        makedirs('tmpfiles/')
-    except OSError:
-        pass
 
     try:
         makedirs(tracklets_path)
     except OSError:
         pass
 
+    try:
+        makedirs(join(tracklets_path, 'tmp'))
+    except OSError:
+        pass
+
     for feat_t in feats_beginend.keys():
         try:
-            makedirs(tracklets_path + feat_t + '/')
+            makedirs(join(tracklets_path, feat_t))
         except OSError:
             pass
 
@@ -92,15 +93,17 @@ def _extract(fullvideonames, videonames, indices, feat_types, tracklets_path):
 
         start_time = time.time()
         # extract the features into temporary file
-        tracklets_filepath = join('tmpfiles/', videonames[i] + '.tmp')
+        tracklets_filepath = join(tracklets_path, 'tmp/', videonames[i] + '.dat')
         if not isfile(tracklets_filepath):
             extract_wang_features(fullvideonames[i], INTERNAL_PARAMETERS['L'], tracklets_filepath)
 
         # read the temporary file to numpy array
+        finput = fileinput.FileInput(tracklets_filepath)
         data = []
-        for line in fileinput.input(tracklets_filepath):
+        for line in finput:
             row = np.array(line.strip().split('\t'), dtype=np.float32)
             data.append(row)
+        finput.close()
 
         try:
             data = np.vstack(data)
@@ -120,7 +123,7 @@ def _extract(fullvideonames, videonames, indices, feat_types, tracklets_path):
 
         # store feature types separately
         for feat_t in feats_beginend.keys():
-            with open(tracklets_path + feat_t + '/' + videonames[i] + '.pkl','wb') as f:
+            with open(join(tracklets_path, feat_t, videonames[i] + '.pkl'),'wb') as f:
                 cPickle.dump(data[:, feats_beginend[feat_t][0]:feats_beginend[feat_t][1]], f)  # TODO: : -> inliners
 
         elapsed_time = time.time() - start_time
@@ -136,13 +139,13 @@ def get_features_beginend(feats_dict, L):
     feats_beginend = {'obj' : (0,                 \
                               feats_dict['obj']), \
                      'trj' : (feats_dict['obj'],                                        \
-                              feats_dict['obj']+(feats_dict['trj']*(L+1))),   \
-                     'hog' : (feats_dict['obj']+(feats_dict['trj']*(L+1)),                              \
-                              feats_dict['obj']+(feats_dict['trj']*(L+1))+feats_dict['hog']), \
-                     'hof' : (feats_dict['obj']+(feats_dict['trj']*(L+1))+feats_dict['hog'],                              \
-                              feats_dict['obj']+(feats_dict['trj']*(L+1))+feats_dict['hog']+feats_dict['hof']), \
-                     'mbh' : (feats_dict['obj']+(feats_dict['trj']*(L+1))+feats_dict['hog']+feats_dict['hof'],                           \
-                              feats_dict['obj']+(feats_dict['trj']*(L+1))+feats_dict['hog']+feats_dict['hof']+feats_dict['mbh'])}
+                              feats_dict['obj']+(feats_dict['trj']*L)),   \
+                     'hog' : (feats_dict['obj']+(feats_dict['trj']*L),                              \
+                              feats_dict['obj']+(feats_dict['trj']*L)+feats_dict['hog']), \
+                     'hof' : (feats_dict['obj']+(feats_dict['trj']*L)+feats_dict['hog'],                              \
+                              feats_dict['obj']+(feats_dict['trj']*L)+feats_dict['hog']+feats_dict['hof']), \
+                     'mbh' : (feats_dict['obj']+(feats_dict['trj']*L)+feats_dict['hog']+feats_dict['hof'],                           \
+                              feats_dict['obj']+(feats_dict['trj']*L)+feats_dict['hog']+feats_dict['hof']+feats_dict['mbh'])}
     return feats_beginend
 
 # Version using precomputed optical flow (stored in .flo files)
@@ -151,10 +154,13 @@ def extract_wang_features(videofile_path, traj_length, output_features_path):
     argsArray = ['./DenseTrackStab', videofile_path, \
                  '-L', str(traj_length)]  # DenseTrackStab is not accepting parameters, hardcoded the L in there
 
-    f = open(output_features_path,"wb")
-    proc = subprocess.Popen(' '.join(argsArray), cwd=FEATURE_EXTRACTOR_RELPATH, shell=True, stdout=f)
-    proc.communicate()
-    f.close()
+    try:
+        f = open(output_features_path,'wb')
+        proc = subprocess.Popen(' '.join(argsArray), cwd=FEATURE_EXTRACTOR_RELPATH, shell=True, stdout=f)
+        proc.communicate()
+        f.close()
+    except IOError:
+        sys.stderr.write('[Error] Cannot open file for writing: %s\n' % videofile_path)
 
 
 def filter_low_density(data, k=30, r=5):

utils.py
@@ -1,5 +1,79 @@
 import numpy as np
 import copy
+import xmltodict
+
+# ==============================================================================
+# Configuration functions
+# ==============================================================================
+
+def load_XML_config(filepath):
+    """
+    Read the configuration from a .xml file in disk.
+    This sets some paths and the features to use.
+    :param filepath:
+    :return:
+    """
+    config_dict = dict()
+
+    with open(filepath) as fd:
+        xml = xmltodict.parse(fd.read())
+
+    for path in xml['configuration']['path']:
+        config_dict[path['@key']] = path['#text'].encode('utf-8')
+
+    if 'option' in xml['configuration']:
+        if not isinstance(xml['configuration']['option'], list):
+            option = xml['configuration']['option']
+            config_dict[option['@key']] = option['#text'].encode('utf-8')
+        else:
+            for option in xml['configuration']['option']:
+                config_dict[option['@key']] = option['#text'].encode('utf-8')
+        # data type conversions from str to target type
+        if 'num_threads' in config_dict:  # num threads has to be an integer
+            config_dict['num_threads'] = int(config_dict['num_threads'])
+
+    features_list = xml['configuration']['features_list']
+
+    if type(features_list['item']) is unicode:
+        config_dict.setdefault('features_list',[]).append(features_list['item'].encode('utf-8'))
+    else:
+        for item in features_list['item']:
+            feat = item.encode('utf-8')
+            config_dict.setdefault('features_list',[]).append(feat)
+
+    # methods_list = xml['configuration']['methods_list']
+    # if type(methods_list['item']) is unicode:
+    #     config_dict.setdefault('methods_list',[]).append(methods_list['item'].encode('utf-8'))
+    # else:
+    #     for item in methods_list['item']:
+    #         method = item.encode('utf-8')
+    #         config_dict.setdefault('methods_list',[]).append(method)
+
+    return config_dict
+
+
+def get_global_config(xml_config):
+    """
+    Construct some additional (output) paths from the xml configuration.
+    :param xml_config:
+    :return:
+    """
+    parent_path = xml_config['data_path'] + xml_config['dataset_name'] + '/'
+    if not isdir(parent_path):
+        makedirs(parent_path)
+
+    tracklets_path = parent_path + 'tracklets/'
+    clusters_path = parent_path + 'clusters/'
+    intermediates_path = parent_path + 'intermediates/'
+    feats_path = parent_path + 'feats/'
+    kernels_path = parent_path + 'kernels/'
+
+    return tracklets_path, clusters_path, intermediates_path, feats_path, kernels_path
+
+
+# ==============================================================================
+# Data structures handling functions
+# ==============================================================================
 
 def merge_dictionaries(dicts):
     """
@@ -59,7 +133,7 @@ def serialize_nested_dictionary(nested_dict):
     return serialized_dict
 
 
-def sum_of_arrays(arrays, weights=None, norm=None, gamma=None):
+def sum_of_arrays(arrays, weights=None, norm=None):
     if weights is None:
         weights = [1.0/len(arrays)] * len(arrays)
     if norm is 'median':
@@ -100,15 +174,15 @@ def sum_of_arrays(arrays, weights=None, norm=None, gamma=None):
 #
 #     return K, p
 
-def normalize(K, type='mean'):
+def normalize(K, type='median'):
     p = argnormalize(K, type=type)
     return p * K
 
-def normalization(K, type='mean'):
+def normalization(K, type='median'):
     p = argnormalize(K, type=type)
     return p * K, p
 
-def argnormalize(K,type='mean'):
+def argnormalize(K,type='median'):
     p = 1.0
 
     values = K[K != 0]
@@ -119,3 +193,27 @@ def argnormalize(K,type='mean'):
             p = 1.0/np.nanmedian(values)
 
     return p
+
+def uniform_weights_dist(n_weights, step=0.1):
+    D = []
+    w = n_weights * [0.]
+    pos = 0
+    acc_weight = 0.
+
+    stack = []
+    stack.append((w,pos,acc_weight))
+
+    while len(stack) > 0:
+        w, pos, acc_weight = stack.pop()
+
+        if pos >= len(w)-1 or acc_weight == 1.:
+            w[-1] = max(0, 1. - min(acc_weight,1))
+            D.append(w[:])
+            continue
+
+        for x in np.arange(0, 1.-acc_weight+step, step):
+            ww = w[:]
+            ww[pos] = x
+            stack.append((ww,pos+1,acc_weight+x))
+
+    return D

videodarwin.py
@@ -30,7 +30,9 @@ def normalizeL2(X):
     return X / np.sqrt(np.sum(np.multiply(X,X), axis=1))
 
 def linearSVR(X, c_param, norm=2):
-    if norm == 2:
+    if norm == 1:
+        XX = normalizeL1(X)
+    else:
         XX = normalizeL2(X)
 
     T = X.shape[0] # temporal length
@@ -41,6 +43,11 @@ def linearSVR(X, c_param, norm=2):
     return clf.coef_
 
 def darwin(X, c_svm_param=1):
+    w_fw, w_rv = _darwin(X, c_svm_param=c_svm_param)
+
+    return np.concatenate([w_fw, w_rv])
+
+def _darwin(X, c_svm_param=1):
     '''
     Computes the videodarwin representation of a multi-variate temporal series.
     :param X: a N-by-T matrix, with N the number of features and T the time instants.
@@ -57,4 +64,4 @@ def darwin(X, c_svm_param=1):
     V = np.cumsum(np.flipud(X),axis=0) / one_to_T # reverse videodarwin
     w_rv = linearSVR(rootSIFT(V), c_svm_param, 2)
 
-    return np.concatenate([w_fw, w_rv])
+    return w_fw, w_rv